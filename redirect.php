5fad7a9a7483fb67c6a6815f4f4fabb54b974437
anand13696 <anand.13696@gmail.com>
2018-02-23 21:41:50 +0530

    Added fb link

--- redirect.php
+++ redirect.php
@@ -21,6 +21,11 @@
 	<div class="navbar-fixed">
 		<nav role="navigation"  class="wow bounceInDown">
 			<div class="nav-wrapper">
+				<a href="https://www.facebook.com/SAENITD/" target="_blank" class="brand-logo">
+					<div class="logo-img">
+						<img src="images/sae logo.jpg" width="23%" alt="saenitd logo">
+					</div>
+				</a>
 				<ul class="right hide-on-med-and-down">
 					<li><a href="#home">Home</a></li>
 					<li><a href="#about">About</a></li>
